b8f853d7c1ee828fdca5ecfb21d9dea6c78cd447
niraj prakash <nirajprakash13@gmail.com>
2017-10-31 20:23:58 +0530

    working on nav drawer

--- src/app/containers/full/container-full.component.ts
+++ src/app/containers/full/container-full.component.ts
@@ -4,13 +4,43 @@ import { MdcTemporaryDrawerComponent } from '@angular-mdc/web';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './container-full.component.html',
-  styleUrls:['./container-full.component.scss']
+  styleUrls: ['./container-full.component.scss']
 })
-export class ContainerFullComponent { 
+export class ContainerFullComponent {
+
   isFixed: boolean = true;
+
   @ViewChild('drawer') drawer: MdcTemporaryDrawerComponent;
 
+  _vmNavItems: any = {
+    dashboard: [
+      {
+        name: "home",
+        icon: "home",
+        color: "mdc-color-text--purple"
+      }, {
+        name: "charts",
+        icon: "insert_charts",
+        color: "mdc-color-text--brown",
+
+      }, {
+        name: "tables",
+        icon: "list",
+        color: "mdc-color-text--green",
+      }, {
+        name: "forms",
+        icon: "mode_edit",
+        color: "mdc-color-text--indigo",
+      }, {
+        name: "widgets",
+        icon: "widgets",
+        color: "mdc-color-text--blue-grey-700",
+      }
+
+    ]};
+
+
   toggleDrawer() {
-    this.drawer.open();
+      this.drawer.open();
+    }
   }
-}
